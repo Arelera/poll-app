9d5c1d2617219c26b9e62452e51305ed764fbeb2
Arel <arelcode@gmail.com>
2020-11-07 16:08:54 +0300

    Trying to fix heroku problem

--- backend/client.js
+++ backend/client.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
-const { USER, PASSWORD, HOST, PORT, DATABASE } = require('./config');
+const { USER, PASSWORD, HOST, DB_PORT, DATABASE } = require('./config');
 
 const client = new Pool({
   user: process.env.DB_USER || USER,
   password: process.env.DB_PASSWORD || PASSWORD,
   host: process.env.DB_HOST || HOST,
-  port: process.env.DB_PORT || PORT,
+  port: process.env.DB_PORT || DB_PORT,
   database: process.env.DB || DATABASE,
 });
 
--- backend/config.js
+++ backend/config.js
@@ -4,6 +4,6 @@ module.exports = {
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
   HOST: process.env.DB_HOST,
-  PORT: process.env.DB_PORT,
+  DB_PORT: process.env.DB_PORT,
   DATABASE: process.env.DB_DB,
 };
